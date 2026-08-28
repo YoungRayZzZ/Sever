ff65bb7d2bacded9b347eb4f33b0bd43becae79e
YoungRayZzZ <nguyennhatanh2211208@gmail.com>
2026-08-28 15:44:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const MAX_CHAT  = 5000;  // /chat-logs viewer
 
 const usersDB = {
     "trap":   { password: "1234",   expire_at: "2026-08-29T23:59:59+07:00" },
-    "thinh":  { password: "thinhh", expire_at: "2026-08-28T21:39:00+07:00" },
-    "peak":   { password: "1002",   expire_at: "2026-08-28T21:39:00+07:00" },
-    "lumi":   { password: "1003",   expire_at: "2026-08-28T23:59:59+07:00" },
-    "noname": { password: "10314",  expire_at: "2026-08-21T23:59:59+07:00" },
-    "dyo":    { password: "1005",   expire_at: "2026-08-28T23:59:59+07:00" }
+    "thinh":  { password: "thinhh", expire_at: "2026-08-29T21:39:00+07:00" },
+    "peak":   { password: "1002",   expire_at: "2026-08-29T21:39:00+07:00" },
+    "lumi":   { password: "1003",   expire_at: "2026-08-29T23:59:59+07:00" },
+    "noname": { password: "10314",  expire_at: "2026-08-29T23:59:59+07:00" },
+    "belanh":    { password: "2222",   expire_at: "2026-08-29T23:59:59+07:00" }
 };
 
 const logHistory  = [];
