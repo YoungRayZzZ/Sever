615e6285a7b674e1d9cfb32fa468b81072c30144
YoungRayZzZ <nguyennhatanh2211208@gmail.com>
2026-09-04 23:01:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ const MAX_CHAT  = 200;  // /chat-logs viewer
 
 const usersDB = {
     "trap":   { password: "1234",   expire_at: "2026-09-30T23:59:59+07:00" },
-    "thinh":  { password: "thinhh", expire_at: "2026-09-02T21:39:00+07:00" },
-    "peak":   { password: "1002",   expire_at: "2026-09-24T21:39:00+07:00" },
-    "lumi":   { password: "1003",   expire_at: "2026-09-24T23:59:59+07:00" },
-    "noname": { password: "10314",  expire_at: "2026-09-24T23:59:59+07:00" },
-    "belanh":    { password: "2222",   expire_at: "2026-09-05T23:59:59+07:00" },
-    "dyo":    { password: "1005",   expire_at: "2026-09-05T23:59:59+07:00" },
-    "linh":    { password: "1005",   expire_at: "2026-09-02T23:59:59+07:00" }
+    "thinh":  { password: "thinhh", expire_at: "2026-03-02T21:39:00+07:00" },
+    "peak":   { password: "1002",   expire_at: "2026-03-24T21:39:00+07:00" },
+    "lumi":   { password: "1003",   expire_at: "2026-03-24T23:59:59+07:00" },
+    "noname": { password: "10314",  expire_at: "2026-03-24T23:59:59+07:00" },
+    "belanh":    { password: "2222",   expire_at: "2026-03-05T23:59:59+07:00" },
+    "dyo":    { password: "1005",   expire_at: "2026-03-05T23:59:59+07:00" },
+    "linh":    { password: "1005",   expire_at: "2026-03-02T23:59:59+07:00" }
 };
 
 const logHistory  = [];
